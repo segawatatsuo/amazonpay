d98c4ecd9a08c8ea44c6034e308e8c9a766dc9a8
segawatatsuo <base@lookingfor.jp>
2025-05-23 23:20:49 +0900

    05-23 23:20

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,2 @@
 <?php
 
-{{-- Laravel内ページでのみ表示（WordPress用JSは除外） --}}
-@if (!request()->is('news/*'))
-    {{-- WordPressのJSは読み込まない --}}
-@else
-    <script src="https://www.ccmedico.com/news/wp-content/themes/montblanc/js/header.js?ver=1.0"></script>
-@endif
