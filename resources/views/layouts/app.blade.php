64d029266b3e16c29c259d873fe4f5cd66548682
segawatatsuo <base@lookingfor.jp>
2025-05-23 16:58:39 +0900

    05-23 16:58

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,2 +1,8 @@
-<!-- jQueryを先に読み込む -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<?php
+
+{{-- Laravel内ページでのみ表示（WordPress用JSは除外） --}}
+@if (!request()->is('news/*'))
+    {{-- WordPressのJSは読み込まない --}}
+@else
+    <script src="https://www.ccmedico.com/news/wp-content/themes/montblanc/js/header.js?ver=1.0"></script>
+@endif
